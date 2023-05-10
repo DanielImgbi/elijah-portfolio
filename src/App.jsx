1e268ebd65c38ec73ec42dd03be0f19582c8e2dc
DivineDomokuma10 <divinedomokuma@gmail.com>
2023-05-10 19:00:39 +0100

    added an adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Home from "./components/Home";
 function App() {
   return (
     <main className="w-screen h-screen bg-no-repeat bg-cover bg-[url('/writing_bg7.jpg')]">
-      <div className="w-full h-[10vh] bg-[rgba(0,0,0,0.75)] text-3xl flex items-center justify-center text-white">Nav</div>
       <Home />
     </main>
   );
