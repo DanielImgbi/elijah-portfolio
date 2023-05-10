a2e6aa5c9d2f41621492fa3033208025f3ed8d24
DivineDomokuma10 <divinedomokuma@gmail.com>
2023-05-10 15:33:06 +0100

    implement Type Effect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,54 @@
 import { FaArrowDown } from "react-icons/fa";
 import { motion } from "framer-motion";
+import { useState, useLayoutEffect } from "react";
 
 const Home = () => {
+  const [skill, setSkill] = useState("");
+  const skills = [
+    "I'm a Freelancer. ",
+    "I'm a Content Writer. ",
+    "I'm a Graphics Designer. ",
+  ];
+  let i = 0;
+  let e = 0;
+  let isComplete = false;
+  let canDelete = false;
+  let holder = "";
+  useLayoutEffect(() => writeEffect(), []); 
+
+  // Writting Effect Algorithm
+  const writeEffect = () => {
+    setInterval(() => {
+      const test = (text) => setSkill(skill + text);
+      if (i < skills.length) {
+        let curSkill = skills[i];
+        if (e < curSkill.length && isComplete === false) {
+          holder += curSkill[e];
+          test(holder);
+          e++;
+          if (e === curSkill.length) {
+            isComplete = true;
+            canDelete = true;
+          }
+        }
+        else if(e !== 0 && canDelete === true){
+          holder = holder.slice(0,e - 2);
+          test(holder)
+          e--
+        }
+        else{
+          isComplete = false;
+          canDelete = false;
+          i++;
+        }
+  
+      }
+      else{
+        i = 0
+      }
+    }, 350);
+  };
+
   return (
     <main
       id="#home"
@@ -9,17 +56,22 @@ const Home = () => {
     >
       <h2 className="text-xl md:text-4xl">Welcome</h2>
       <h1 className=" flex items-center font-semibold text-3xl space-x-1 md:space-x-3 md:text-7xl md:font-bold">
-        <span>I&apos;m a Graphics Designer</span>
-        <div className="w-1 h-11 md:h-20 bg-white mt-2"></div>
+        <span>{skill}</span>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ repeat: Infinity, delay: 0.5 }}
+          className="w-1 h-11 md:h-20 bg-white mt-2 md:w-2"
+        ></motion.div>
       </h1>
       <p className=" text-gray-300 md:text-2xl">
         based in PortHarcourt, Nigeria.
       </p>
-      <button className="text-green-600 px-7 py-3 rounded-full border border-green-600 hover:bg-green-600 hover:text-white hover:font-semibold">
+      <a href="#" className="text-green-600 px-7 py-3 rounded-full border border-green-600 hover:bg-green-600 hover:text-white hover:font-semibold">
         Hire Me
-      </button>
+      </a>
       <motion.a
-        href=""
+        href="#"
         initial={{ y: 5 }}
         animate={{ y: 120 }}
         transition={{ delay: 2.5, duration: 4, repeat: Infinity }}
